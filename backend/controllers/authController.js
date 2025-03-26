89292c256ad2c7f63a7cfa693fdc8c4bb46f91fe
devinda.jay <devinda.jay.me@gmail.com>
2025-03-26 11:33:06 +0530

    set up functions to register a new user

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -10,8 +10,37 @@ const generateToken = (id) =>{
 
 //register user
 module.exports.registerUser = async (req,res) =>{
-    
+    try {
+        const {fullName, email, password, profileImageUrl } = req.body; 
+
+        //checking for missing fields
+        if(!fullName || !email || !password){
+            return res.status(400).json({message: 'Please provide all fields'});
+        }
+        //check if user already exists
+        const existingUser = await User.findOne({email});
+        if(existingUser){
+            return res.status(400).json({message: 'User already registered!'});
+        }
+        //create a new user 
+        const user = await User.create({
+            fullName,
+            email,
+            password,
+            profileImageUrl
+        })
+        res.status(201).json({
+            id: user._id,
+            fullName: user.fullName,
+            email: user.email,
+            token: generateToken(user._id)
+        }); 
+    } catch (error) {
+        console.error('User registration failed!!', error);
+        res.status(500).json({message: 'User registration failed'});
+    }
 }
+
 //login user
 module.exports. loginUser = async (req,res) =>{
 
