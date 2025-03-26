343590e5bef3ffbe81f17983a9fe1be846d4c6b8
devinda.jay <devinda.jay.me@gmail.com>
2025-03-26 22:39:44 +0530

    set up database schema to handle income

---
+++ backend/controllers/incomeController.js
@@ -0,0 +1,15 @@
+const User = require('../models/User');
+const Income = require('../models/Income');
+
+//Add Income Source
+module.exports.addIncome = async (req,res) =>{
+}
+//Get Income Sources
+module.exports.getIncomes = async (req,res) =>{
+}
+//Delete Income Source
+module.exports.deleteIncome = async (req,res) =>{
+}
+//Download Income Source
+module.exports.downloadIncomeExcel = async (req,res) =>{
+}
