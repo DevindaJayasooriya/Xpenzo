1aafc41b301c9f63dab2501ad6be9f8ab4c8b1be
devinda.jay <devinda.jay.me@gmail.com>
2025-03-26 22:59:48 +0530

    setup function to add a income

--- backend/controllers/incomeController.js
+++ backend/controllers/incomeController.js
@@ -3,6 +3,30 @@ const Income = require('../models/Income');
 
 //Add Income Source
 module.exports.addIncome = async (req,res) =>{
+    const userId = req.user.id;
+
+    const{icon,source,amount,date} = req.body;
+
+    //check if missing fields
+    if(!source || !amount || !date){
+        return res.status(400).json({msg:'Please fill all fields'});
+    }
+
+    try {
+        const newIncome = new Income({
+            userId,
+            icon,
+            source,
+            amount,
+            date:new Date(date),
+        });
+
+        const income = await newIncome.save();
+        res.json(income);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({msg:'Server Error'});
+    }
 }
 //Get Income Sources
 module.exports.getIncomes = async (req,res) =>{
