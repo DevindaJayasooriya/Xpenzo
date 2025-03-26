7d360115ef07b882eae11870ae76cd2d24a52d3b
devinda.jay <devinda.jay.me@gmail.com>
2025-03-26 17:17:08 +0530

    added login functions

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -40,7 +40,29 @@ module.exports.registerUser = async (req,res) =>{
 
 //login user
 module.exports. loginUser = async (req,res) =>{
+    const {email, password} = req.body;
+    try{
+        //check for missing fields
+        if(!email || !password){
+            res.status(400).json({message: "Please provide email and password"});
+        }
 
+        //check user exists
+        const user = await User.findOne({email});
+        if(!user || !(await user.comparePassword(password))){
+            return res.status(400).json({message: "Invalid credentials"});
+        }
+
+        res.status(200).json({
+            id: user._id,
+            user,
+            token: generateToken(user._id)
+        });
+        
+    } catch (error) {
+        console.error('User login failed!!', error);
+        res.status(500).json({message: 'User login failed'});
+    }
 }
 //getUserInfo 
 module.exports. getUserInfo = async (req,res) =>{
